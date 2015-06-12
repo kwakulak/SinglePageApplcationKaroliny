cbb84cef7b3e2b0784ae96f4a6d2256dcc09938d
Karolina Wakulak <wakulakkarolina@gmail.com>
2015-06-12 11:00:27 +0200

    Przed dodaniem edycji listy

--- SinglePageApplicationKaroliny/Models/Recipe.cs
+++ SinglePageApplicationKaroliny/Models/Recipe.cs
@@ -11,7 +11,7 @@ namespace SinglePageApplicationKaroliny.Models
         public RecipeCategory Category { get; set; }
         public string Title { get; set; }
         public string Description { get; set; }
-        public List<string> IngredientsList {get; set; }
+        public IList<string> IngredientsList {get; set; }
         public DateTime AddDate {get; set;}
         public string PictureURL { get; set; }
     }
