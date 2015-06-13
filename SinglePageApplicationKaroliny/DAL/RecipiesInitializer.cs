807defe3fb70d0a1de594f733be2ce683a52e6c8
Dabler <podbielski.grzegorz@gmail.com>
2015-06-13 19:27:30 +0200

    Centered title

--- SinglePageApplicationKaroliny/DAL/RecipiesInitializer.cs
+++ SinglePageApplicationKaroliny/DAL/RecipiesInitializer.cs
@@ -10,13 +10,13 @@ namespace SinglePageApplicationKaroliny.DAL
     {
         protected override void Seed(RecipiesContext context)
         {
-            var students = new List<Recipe>
-            {
-            new Recipe{Category=RecipeCategory.Drink, Title="Alexander", Description="Famous dring created from cream, brandy and chocolate liquer", IngredientsList={"Brandy", "Chocolate Liqeur", "Cream", "Ice"}, AddDate = DateTime.Parse("2005-09-01"), PictureURL="http://farm9.staticflickr.com/8479/8246034506_029fd92a96_o.jpg"}
-            };
+//            var students = new List<Recipe>
+//            {
+//            new Recipe{Category=RecipeCategory.Drink, Title="Alexander", Description="Famous dring created from cream, brandy and chocolate liquer", IngredientsList={"Brandy", "Chocolate Liqeur", "Cream", "Ice"}, AddDate = DateTime.Parse("2005-09-01"), PictureURL="http://farm9.staticflickr.com/8479/8246034506_029fd92a96_o.jpg"}
+//            };
 
-            students.ForEach(s => context.Students.Add(s));
-            context.SaveChanges();
+//            students.ForEach(s => context.Students.Add(s));
+//            context.SaveChanges();
         }
     }
 }
