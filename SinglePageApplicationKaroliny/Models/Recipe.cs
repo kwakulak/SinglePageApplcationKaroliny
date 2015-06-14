6c84844243e3a8873b54062e3b256e30b4a82a07
Dabler <podbielski.grzegorz@gmail.com>
2015-06-14 22:02:21 +0200

    Changes in index layout

--- SinglePageApplicationKaroliny/Models/Recipe.cs
+++ SinglePageApplicationKaroliny/Models/Recipe.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -12,6 +13,7 @@ namespace SinglePageApplicationKaroliny.Models
         public string Title { get; set; }
         public string Description { get; set; }
         public IList<string> IngredientsList {get; set; }
+        [DisplayFormat(DataFormatString = "{0:dd/MM/yyyy}", ApplyFormatInEditMode = true)]
         public DateTime AddDate {get; set;}
         public string PictureURL { get; set; }
     }
